6980e5345facc378220c72d63ba80788b4f9b19b
study-sp0ts <studispo0ts@gmail.com>
2026-03-25 12:35:43 +0100

    Your commit message here

--- src/pages/LoginSignup.jsx
+++ src/pages/LoginSignup.jsx
@@ -17,45 +17,45 @@ const LoginSignup = () => {
       <div className={`container ${rightPanelActive ? 'right-panel-active' : ''}`} id="container">
         <div className="form-container sign-up-container">
           <form action="#">
-            <h1>Create Account</h1>
-            <div className="social-container">
+            <h1>Profil erstellen</h1>
+            {/* <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
               <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-            </div>
-            <span>or use your email for registration</span>
+            </div> 
+            <span>or use your email for registration</span> */}
             <input type="text" placeholder="Name" />
             <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <button>Sign Up</button>
+            <input type="password" placeholder="Passwort" />
+            <button>Registrieren</button>
           </form>
         </div>
         <div className="form-container sign-in-container">
           <form action="#">
-            <h1>Sign in</h1>
-            <div className="social-container">
+            <h1>Einloggen</h1>
+            {/* <div className="social-container">
               <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
               <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
               <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-            </div>
-            <span>or use your account</span>
+            </div> 
+            <span>or use your account</span> */}
             <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
-            <a href="#">Forgot your password?</a>
-            <button>Sign In</button>
+            <input type="password" placeholder="Passwort" />
+            <a href="#">Passwort vergessen?</a>
+            <button>Einloggen</button>
           </form>
         </div>
         <div className="overlay-container">
           <div className="overlay">
             <div className="overlay-panel overlay-left">
-              <h1>Welcome Back!</h1>
-              <p>To keep connected with us please login with your personal info</p>
-              <button className="ghost" onClick={handleSignInClick} id="signIn">Sign In</button>
+              <h1>Wilkommen zurück!</h1>
+              <p>Um auf alle Fuktionen zugriff zu haben, logge dich bitte mit deinen Anmeldedaten ein</p>
+              <button className="ghost" onClick={handleSignInClick} id="signIn">Einloggen</button>
             </div>
             <div className="overlay-panel overlay-right">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start journey with us</p>
-              <button className="ghost" onClick={handleSignUpClick} id="signUp">Sign Up</button>
+              <h1>Hallo, Freund!</h1>
+              <p>Erzähl uns ein wenig über dich und starte unser gemeinsames Kapitel</p>
+              <button className="ghost" onClick={handleSignUpClick} id="signUp">Registrieren</button>
             </div>
           </div>
         </div>
